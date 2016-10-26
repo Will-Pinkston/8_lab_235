ee332663026c96c5635f09fb80c46cd51e1666a2
Will Pinkston <william@pinkston.com>
2016-10-25 19:02:20 -0600

    edited out prints

--- Student_Code/QS.cpp
+++ Student_Code/QS.cpp
@@ -41,11 +41,11 @@ void sortItR(int left, int right, QS* myQS) {
     }
     int pivot = myQS->medianOfThree(left, right);
     int partitionMid = myQS->partition(left, right, pivot);
-    cout << myQS->getArray() << endl << endl;
+//    cout << myQS->getArray() << endl << endl;
     sortItR(left, partitionMid - 1, myQS);
-    cout << myQS->getArray() << endl << endl;
+//    cout << myQS->getArray() << endl << endl;
     sortItR(partitionMid + 1, right, myQS);
-    cout << myQS->getArray() << endl << endl;
+//    cout << myQS->getArray() << endl << endl;
     return;
 }
 //////
